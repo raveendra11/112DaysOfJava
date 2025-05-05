b886e2c1198cf911a4b9144b833dd4f0d398503d
raveendra11 <Gurthuledu@1>
2025-05-05 18:41:52 -0500

    A program on Java arithmetic operations.

---
+++ Operators/ArithmeticOperators.java
@@ -0,0 +1,21 @@
+package Operators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int number1 = 2;
+        int number2 = 3;
+
+        //addition
+        System.out.println(number1 + number2);
+        //subtraction
+        System.out.println(number1 - number2);
+        //multiplication
+        System.out.println(number1 * number2);
+        //division
+        System.out.println(number2 / number1);
+        //modulo
+        System.out.println(number1 % number2);
+
+    }
+}
