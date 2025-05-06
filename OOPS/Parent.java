644257a1afe8833d6a5beeeddd40b28d585e2611
raveendra11 <Gurthuledu@1>
2025-05-06 09:56:06 -0500

    A program on Java OOPS having Inheritence example.

---
+++ OOPS/Parent.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+public class Parent {
+    int age = 27;
+    String name = "Ish";
+
+    public void doPrint() {
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
