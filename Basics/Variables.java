39b3f7382c7c6368f1b8af5265623348b31ac1ef
raveendra11 <Gurthuledu@1>
2025-05-14 15:57:49 -0500

    A program on Java Variables.

--- Basics/Variables.java
+++ Basics/Variables.java
@@ -2,15 +2,26 @@ package Basics;
 
 public class Variables {
 
+    //Instance variables
+    //Declaration + Initialization (combined)
+    int number = 10;
+    boolean lightOn = true;
+
+    //Static variable
+    static String College = "USD";
+
+    public void doPrint() {
+        //Local variable
+        boolean isPrintable = true;
+        if (isPrintable)
+            System.out.println("Printing....");
+    }
+
     public static void main(String[] args) {
         //Declaration
         char letter;
 
         //Initialization
         letter = 'a';
-
-        //Declaration + Initialization (combined)
-        int number =10;
-        boolean lightOn = true;
     }
 }
