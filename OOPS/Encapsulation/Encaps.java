8c5ae1531e98126d6c243b190095725bf30a16b9
raveendra11 <Gurthuledu@1>
2025-05-16 08:44:49 -0500

    A program on Java Important Keywords.

---
+++ OOPS/Encapsulation/Encaps.java
@@ -0,0 +1,20 @@
+package OOPS.Encapsulation;
+
+public class Encaps {
+
+    String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+       this.name=name;
+    }
+
+    public static void main(String[] args) {
+            Encaps object = new Encaps();
+            object.setName("Ishu");
+        System.out.println(object.getName());
+    }
+}
