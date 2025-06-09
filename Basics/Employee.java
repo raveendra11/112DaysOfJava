059cb03332fefbbe9139deed81a574e50c25a1e7
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2025-06-08 22:24:49 -0500

    A program on Java Objects.

--- Basics/Employee.java
+++ Basics/Employee.java
@@ -3,9 +3,11 @@ package Basics;
 public class Employee {
     int id;
     String name;
+    double salary;
 
-    public void insertEmployee(int id, String name) {
+    public void insertEmployee(int id, String name, double salary) {
         this.id = id;
         this.name = name;
+        this.salary=salary;
     }
 }
