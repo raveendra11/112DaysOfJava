77d1edfe5058a25fa326118885f94fa5049c9668
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2025-06-10 18:48:51 -0500

    A program on Java Keywords - static.

---
+++ Keywords/Static.java
@@ -0,0 +1,14 @@
+package Keywords;
+
+public class Static {
+
+    static String name = "Ishu";
+
+    public static void doPrint(){
+        System.out.println("I'm really sorry,"+name+"!");
+    }
+
+    public static void main(String[] args) {
+        doPrint();
+    }
+}
