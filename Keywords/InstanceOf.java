972cbd38769ca28b68c2191916b41ec932c1a7a4
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2025-06-12 00:29:43 -0500

    A program on Java Keywords - InstanceOf.

---
+++ Keywords/InstanceOf.java
@@ -0,0 +1,8 @@
+package Keywords;
+
+public class InstanceOf {
+    public static void main(String args[]) {
+        InstanceOf s = new InstanceOf();
+        System.out.println(s instanceof InstanceOf);
+    }
+}
