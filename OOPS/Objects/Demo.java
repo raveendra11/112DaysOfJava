b7ea9b4f0a92c59ccae9c7cf86ded0a027eb767a
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2025-06-17 10:52:56 -0500

    A program on Java OOPS - Object.

---
+++ OOPS/Objects/Demo.java
@@ -0,0 +1,14 @@
+package OOPS.Objects;
+
+public class Demo {
+
+    public void showDemo() {
+        System.out.println("Welcome to java objects");
+    }
+
+    public static void main(String[] args) {
+        Demo object = new Demo();
+        object.showDemo();
+    }
+
+}
