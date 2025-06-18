e61f5743db6924ac4af36be1fb8f1cc4314d3427
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2025-06-18 21:58:50 +0530

    A program on Java Comments.

---
+++ Basics/Comments.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class Comments {
+
+    // This is a single line comment.
+    public static void main(String[] args) {
+        System.out.println("Java comments"); //printing the String "Java comments"
+
+        /* Let's declare and
+ print variable in java.
+ Is a Multi line comment*/
+        int i = 10;
+        System.out.println(i);
+
+    }
+}
