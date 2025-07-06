01f256ade437fb5f5e7d3f8d4084a436015374c1
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2025-07-06 22:02:01 +0530

    Update Single.java

--- Arrays/Single.java
+++ Arrays/Single.java
@@ -9,6 +9,9 @@ public class Single {
         for (int x : numbers) {
             System.out.println(x);
         }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 
     public static void main(String[] args) {
