97b4634e36b927e80f77b6acccbdf7bffc43e396
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2025-07-16 21:58:22 +0530

    Update OptionalClass.java

--- Java8/OptionalClass.java
+++ Java8/OptionalClass.java
@@ -5,7 +5,7 @@ public class OptionalClass {
     public static void main(String[] args) {
         String[] str = new String[10];
         Optional<String> checkNull = Optional.ofNullable(str[5]);
-        if(checkNull.isPresent()){  // check for value is present or not
+        if(checkNull.isPresent()){ 
             String lowercaseString = str[5].toLowerCase();
             System.out.print(lowercaseString);
         }else
