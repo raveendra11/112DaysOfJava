cd7afca0d854879322520b1a7b883b060e8df1ab
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2025-07-20 21:34:17 +0530

    Update StringClass.java

--- Strings/StringClass.java
+++ Strings/StringClass.java
@@ -4,8 +4,8 @@ public class StringClass {
 
     public static void main(String[] args) {
 
-        String firstName = "Ishu";
-        String lastName = new String("Ishu");
+        String firstName = "Ishu"; //in SCP area
+        String lastName = new String("Ishu"); //Stored in Heap area
 
         System.out.println(firstName);
         System.out.println(lastName);
