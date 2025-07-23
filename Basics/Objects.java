e3e1b7c4017d9814396c2d65de2a611f1e1479ec
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2025-07-23 21:19:57 +0530

    Update Objects.java

--- Basics/Objects.java
+++ Basics/Objects.java
@@ -4,6 +4,7 @@ public class Objects {
 
     int age = 27;
     String name = "Ishu";
+    String gender = "female";
 
     public static void main(String[] args) {
         //object creation
@@ -12,5 +13,6 @@ public class Objects {
         //printing age and name with object
         System.out.println(object.age);
         System.out.println(object.name);
+        System.out.println(object.gender);
     }
 }
