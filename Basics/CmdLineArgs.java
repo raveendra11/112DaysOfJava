d4947cbccde52b263864bed3cee4d25e21db04b9
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2025-08-05 17:32:18 +0530

    A program on Java for Command Line Arguments.

---
+++ Basics/CmdLineArgs.java
@@ -0,0 +1,8 @@
+package Basics;
+
+public class CmdLineArgs {
+        public static void main(String args[]) {
+            //Pass this arg through cmd. e.g., Ishu
+            System.out.println("Your first argument is: "+args[0]);
+        }
+    }
