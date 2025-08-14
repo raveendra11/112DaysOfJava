ecf3e960588eefe9a34963c0bee5d83ca5790324
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2025-08-14 23:26:14 +0530

    Update StringClass.java

--- Strings/StringClass.java
+++ Strings/StringClass.java
@@ -11,6 +11,10 @@ public class StringClass {
         String name = "Ravi";
         //Stored in Heap area
         String lastname = new String("Pujari");  
+        //in SCP area
+        String name = "Teja";
+        //Stored in Heap area
+        String lastname = new String("Sada");  
 
         System.out.println(firstName);
         System.out.println(lastName);
