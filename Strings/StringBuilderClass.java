1320ccc101e799ba568b5e161e379d5d12811ded
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2025-08-19 04:16:43 +0530

    Create StringBuilderClass.java

---
+++ Strings/StringBuilderClass.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class StringClass {
+  public static void main(String args[]){
+    StringBuilder object = new StringBuilder("Ishu");
+    System.out.println("Initial StringBuilder: " + sb);
+
+        // Append a string to the StringBuilder
+        sb.append(" is a good girl!");
+        System.out.println("After append: " + sb);
+  }
+} 
